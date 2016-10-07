56e8eb9b00ce08384416a65924ca2ea56d813fba
Adam Hill <adam.graham.hill@gmail.com>
2016-10-07 15:25:35 -0400

    Set-up tumblr client config

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require_relative 'config/tumblr_client.rb'
 
 get '/' do
 	"Mic check ah ha ha"
--- config/tumblr_client.rb
+++ config/tumblr_client.rb
@@ -4,8 +4,8 @@ require 'tumblr_client'
 Dotenv.load
 
 Tumblr.configure do |config|
-  config.consumer_key = ""
-  config.consumer_secret = ""
-  config.oauth_token = ""
-  config.oauth_token_secret = ""
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.oauth_token = ['OAUTH_TOKEN']
+  config.oauth_token_secret = ['OAUTH_TOKEN_SECRET']
 end
