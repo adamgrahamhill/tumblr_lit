97f25a07bc2a498736fb9f085d15eb016017148c
Adam Hill <adam.graham.hill@gmail.com>
2016-10-14 20:16:35 -0400

    Fix config, test request for info/posts successful

--- app.rb
+++ app.rb
@@ -4,3 +4,9 @@ require_relative 'config/tumblr_client.rb'
 get '/' do
 	"Mic check ah ha ha"
 end
+
+get '/test' do
+	client = Tumblr::Client.new
+	posts = client.posts("sumohill.tumblr.com")  #see if I can get a response from my account
+	"#{client.info} and\n #{posts}"
+end
