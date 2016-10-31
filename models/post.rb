9650cfceb876f7d7ef0a9efa3d76ddefabdeb38d
Adam Hill <adam.graham.hill@gmail.com>
2016-10-30 21:01:27 -0400

    Update so posts are correctly accessed, removes ['posts'] from view

--- models/post.rb
+++ models/post.rb
@@ -2,9 +2,9 @@ require_relative '../config/tumblr_client.rb'
 
 class Post
 
-	def self.get_posts(blog_name, post_type)
+	def self.get_posts(blog_name, post_type) #Requests all posts of specified type from Tumblr api
 		client = Tumblr::Client.new
-		client.posts(blog_name, post_type)
+		client.posts(blog_name, post_type)['posts']
 	end
 
 end
