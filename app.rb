16aa5eb87852ab5320faa1935c10b1303c6c0eb6
Adam Hill <adam.graham.hill@gmail.com>
2016-10-30 21:01:58 -0400

    Add helper method to access image url from a post for use in views

--- app.rb
+++ app.rb
@@ -4,12 +4,18 @@ set :database, {adapter: "postgresql", database: "tumblr_lit.pg"}
 require_relative 'config/tumblr_client.rb'
 require_relative 'models/post'
 
+helpers do
+	def photo_url(post)
+		post['photos'][0]['alt_sizes'][0]['url']
+	end
+end
+
 get '/' do
 	"Mic check ah ha ha"
 end
 
 get '/test' do
 	@posts = Post.get_posts("sumohill", :type => "photo")
-	p @posts
+	#@posts = Post.create_photo_array("sumohill", :type => "photo")
 	erb :index
 end
